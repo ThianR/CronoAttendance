9bd5b9cb002698cf1ed7478456c2e47724bc1e91
ThianR <gabrielrolonth@gmail.com>
2025-08-25 21:20:48 -0300

    orden descendente de findAll

--- backend/src/main/java/com/cronoattendance/services/EmpresasService.java
+++ backend/src/main/java/com/cronoattendance/services/EmpresasService.java
@@ -1,7 +1,10 @@
 package com.cronoattendance.services;
 
+import static org.springframework.data.domain.Sort.Direction.DESC;
+
 import java.util.List;
 
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -16,7 +19,7 @@ public class EmpresasService {
     private final EmpresasRepository EmpresasRepo;
 
     public List<Empresas> findAll() {
-        return EmpresasRepo.findAll();
+        return EmpresasRepo.findAll(Sort.by(DESC, "id"));
     }
 
     public Empresas findById(Integer id) {
